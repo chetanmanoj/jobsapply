2b9369d77b9085cf002337e205bfb6a907693f36
chetanmanoj <chetanmanoj2001@gmail.com>
2024-04-04 12:38:54 +0400

    First commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const PORT = 8000;
+
+app.use(
+  cors({
+    origin: " http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(PORT, () =>
+  console.log(`app listening on http://127.0.0.1:${PORT}/api/`)
+);
